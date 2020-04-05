6e266272fe722d2998d0d6dece7d524745b54410
seoful <seoful@yandex.ru>
2020-04-06 00:57:55 +0500

    d

--- bot.py
+++ bot.py
@@ -8,13 +8,12 @@ import schedule
 
 API_KEY = "1103395186:AAEjPT2Yo0Nc5KSGoJgYuDAbQdIXGTix0ys"
 bot = telebot.TeleBot(API_KEY)
-ff = None
 CREATOR_CHAT_ID = 377263029
 
 
 def get_photo(request):
     response = requests.get(
-        "https://api.unsplash.com/photos/random?client_id=xqeRzqIeuPGTeFeRtw3_WvrAiGuce6p4-eTU9BuWZUs&count=1&query="+request)
+        "https://api.unsplash.com/photos/random?client_id=xqeRzqIeuPGTeFeRtw3_WvrAiGuce6p4-eTU9BuWZUs&count=1&query=" + request)
     if (response.ok):
         response = json.loads(response.text)
         response = response[0]["urls"]["regular"]
@@ -23,10 +22,30 @@ def get_photo(request):
         return None
 
 
-def send_photo_every_minute():
-    photo = get_photo()
+photo = get_photo("cat")
+
+
+def download_photo():
+    global photo
+    photo = get_photo("cat")
+
+
+def scheduled_photo():
     for chat_id in get_ids():
-        bot.send_photo(chat_id, photo)
+        if photo is not None:
+            bot.send_photo(chat_id, photo)
+        else:
+            bot.send_message(chat_id,
+                             "Error getting photo.Sorry( Maybe,we`ve run out of requests. Wait for an hour.")
+
+
+def to_all(admin_id,caption="from admin with love"):
+    local_photo = get_photo("cat")
+    if local_photo is not None:
+        for chat_id in get_ids():
+            bot.send_photo(chat_id, local_photo,caption)
+    else:
+        bot.send_message(admin_id, "Something went wrong")
 
 
 class ScheduleMessage():
@@ -73,15 +92,17 @@ def delete_id(chat_id):
 def send_сat(message):
     photo = get_photo("cat")
     if photo is not None:
-        bot.send_photo(message.chat.id, photo,str(datetime.datetime.now().hour)+str(datetime.datetime.now().minute))
+        bot.send_photo(message.chat.id, photo)
     else:
-        bot.send_message(message.chat.id, "Error getting photo.Sorry( Maybe,we`ve run out of requests. Wait for an hour.")
+        bot.send_message(message.chat.id,
+                         "Error getting photo.Sorry( Maybe,we`ve run out of requests. Wait for an hour.")
 
 
 @bot.message_handler(commands=['subscribe'])
 def subscribe(message):
     if add_id(message.chat.id):
-        bot.send_message(message.chat.id, "Subscribed. To unsubscribe send \\unsubscribe.")
+        bot.send_message(message.chat.id,
+                         "Subscribed to cat photo at 9AM every day. To unsubscribe send \\unsubscribe.")
     else:
         bot.send_message(message.chat.id, "You are already subscribed")
 
@@ -94,8 +115,18 @@ def unsubscribe(message):
         bot.send_message(message.chat.id, "You were not subscribed")
 
 
-schedule.every(1).minute.do(send_photo_every_minute)
+@bot.message_handler(commands=['admin'])
+@bot.message_handler(func=lambda msg: msg.chat.id == CREATOR_CHAT_ID)
+def admin(message):
+    args = message.text.split(" ")[1:]
+    command = args[0]
+    if command == "toall":
+        to_all(message.chat.id,(" ".join(args[1:])))
 
+
+# schedule.every(1).minute.do(send_photo_every_minute)
+schedule.every().day.at("04:00").do(scheduled_photo)
+schedule.every().day.at("19:01").do(download_photo)
 
 if __name__ == '__main__':
     ScheduleMessage.start_process()
